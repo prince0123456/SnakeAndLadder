389a86528bc1846d0f243272a373fbd7891e8c26
Prince Gupta <princegupta.pg46@gmail.com>
2023-03-22 10:41:14 +0530

    [Prince ] check for option

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Xml.Linq;
+
 namespace SnakeAndLadder
 {
     internal class SnakeAndLadder
@@ -8,16 +10,42 @@ namespace SnakeAndLadder
             Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
             SnakeAndLadder playerOne = new SnakeAndLadder();
             playerOne.DieRolling();
-            Console.WriteLine(playerOne.DieRolling());
+            //Console.WriteLine(playerOne.DieRolling());
+            playerOne.PlayerCheck();
         }
 
+        //variable declaration
         public int PlayerPosition = 0;
+        public const int No_Play = 0;
+        public const int Ladder = 1;
+        public const int Snake = 2;
+
         // uc-2 die rolling
         Random random = new Random();
         public int DieRolling()
         {
             int roll = random.Next(1, 7);
             return roll;
+        }
+        //uc3 player-check
+        public void PlayerCheck()
+        {
+            int dieRolling = this.DieRolling();
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case No_Play:
+                    break;
+
+                case Ladder:
+                    this.PlayerPosition += dieRolling;
+                    break;
+                case Snake:
+                    this.PlayerPosition -= dieRolling;
+                    break;
+
+            }
+            Console.WriteLine(this.PlayerPosition);
         }
     }
 }
